7d971d6794fc1ab90326796dd8839b817d304245
Sam Cheesman <s.cheesman1234500@gmail.com>
2019-07-20 13:44:26 +0100

    Added routes for tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # get 'tasks', to: 'task#index'
+  # get 'tasks/:id', to: 'tasks#show'
+  # get 'tasks/new', to: 'task#new'
+  # post 'tasks', to: 'task#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit'
+  # patch 'tasks:id', to: 'task#update'
+  # delete 'tasks:id', to: 'task#destroy'
 end
