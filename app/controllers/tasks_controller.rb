b9b7b4ee422e156e2b6429a824552db5f723e0e9
Sam Cheesman <s.cheesman1234500@gmail.com>
2019-07-20 16:43:02 +0100

    Created CRUD for the controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,39 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def create
+    @task = Task.create(tasks_params)
+    redirect_to tasks_path
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit() end
+
+  def show() end
+
+  def update
+    @task.update(tasks_params)
+    redirect_to tasks_path(@task)
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def tasks_params
+    params.require(:task).permit(:title, :details, :complete)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 end
